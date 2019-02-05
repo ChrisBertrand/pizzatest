7981898379db344575ca6d12abc2ebd5913c40ef
Dan Bryce <danbryce1@gmail.com>
2019-02-05 13:21:35 +0000

    Populate array

--- index.js
+++ index.js
@@ -7,9 +7,27 @@ var dataByLine = data.split("\n");
 
 // Establish parameters
 var firstRow = dataByLine[0].split(" ");
-const rows = firstRow[0];
-const columns = firstRow[1];
-const minIngredients = firstRow[2];
-const maxCells = firstRow[3];
+const rows = Number(firstRow[0]);
+const cols = Number(firstRow[1]);
+const minIngredients = Number(firstRow[2]);
+const maxCells = Number(firstRow[3]);
 
-// TODO Read data into 2D array
+console.log(rows);
+console.log(cols);
+console.log(minIngredients);
+console.log(maxCells);
+
+// Initialise 2D array
+const pizza = new Array(cols);
+for (var x = 0; x < cols; x++) {
+    pizza[x] = new Array(rows);
+}
+
+// Populate array with our pizza data
+for (var y = 0; y < rows; y++) {
+    for (var x = 0; x < cols; x++) {
+        pizza[x][y] = dataByLine[y + 1].charAt(x);
+    }
+}
+
+console.log(pizza);
