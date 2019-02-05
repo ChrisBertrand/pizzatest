8a89ee0871e0de597d2ee25f73e5c0652fcb5695
esummers1 <eddie.summers1@gmail.com>
2019-02-05 13:04:27 +0000

    Read basic information into memory

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-var fs = require('fs');
+var fs = require("fs");
+const inputFile = "./input/a_example.in";
 
-fs.readFile('input', 'ascii', function(err, contents) {
-    console.log(contents);
-});
+// Read data from file
+var data = fs.readFileSync(inputFile, "utf-8");
+var dataByLine = data.split("\n");
+
+// Establish parameters
+var firstRow = dataByLine[0].split(" ");
+const rows = firstRow[0];
+const columns = firstRow[1];
+const minIngredients = firstRow[2];
+const maxCells = firstRow[3];
+
+// TODO Read data into 2D array
