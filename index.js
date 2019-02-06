5d36bf99e13353c1a4e6460e46aa52799f2a930a
Chris Bertrand <cjb_bjc@hotmail.com>
2019-02-06 22:02:55 +0000

    Changing Col,row order

--- index.js
+++ index.js
@@ -12,19 +12,13 @@ const cols = Number(firstRow[1]);
 const minIngredients = Number(firstRow[2]);
 const maxCells = Number(firstRow[3]);
 
-console.log(rows);
-console.log(cols);
-console.log(minIngredients);
-console.log(maxCells);
+//console.log(rows);
+// console.log(cols);
+// console.log(minIngredients);
+// console.log(maxCells);
 
 const slices = [];
 let cellHistory = [];
-
-class Point{
-  constructor(x, y) {
-    this.x = x; this.y = y;
-  }
-}
 
 class slice{
   constuctor() {
@@ -33,25 +27,21 @@ class slice{
 }
 
 // Initialise 2D array
-const pizza = new Array(cols);
-for (var x = 0; x < cols; x++) {
-    pizza[x] = new Array(rows);
+const pizza = new Array(rows);
+
+for (var x = 0; x < rows; x++) {
+    pizza[x] = new Array(cols);
 }
 
 // Populate array with our pizza data
-for (var y = 0; y < rows; y++) {
-    for (var x = 0; x < cols; x++) {
-        pizza[x][y] = dataByLine[y + 1].charAt(x);
+for (var x = 0; x < rows; x++) {
+    for (var y = 0; y < cols; y++) {
+        pizza[x][y] = dataByLine[x + 1].charAt(y);
     }
 }
 
-console.log(pizza);
-
 let outSlices = findSliceVertical();
-
-createOutput(outSlices);
-
-//findSliceHorizontal();
+//createOutput(outSlices);
 
 function createOutput(slices) {
   console.log(slices);
@@ -59,50 +49,6 @@ function createOutput(slices) {
   wstream.write(slices.length + '\n');
   slices.forEach(s => wstream.write(`${s.r1} ${s.c1} ${s.r2} ${s.c2} \n`));
   wstream.end();
-}
-
-function findSliceHorizontal() {
-  let numMushrooms = 0;
-  let numTomatoes = 0;
-
-  let s = new slice();
-
-  for (var row = 0; row < rows ; row++) {
-    for (var col = 0; col < cols; col++) {
-
-      let ingred = pizza[col][row];
-
-      if (ingred == 'M' || ingred == 'T') {
-        pizza[col][row] = slices.length > 0 ? slices.length : ingred;
-        console.log(row, col, ingred);
-        s.r1 = row; s.c1 = col;
-      }
-
-      if (ingred == 'M') {
-        numMushrooms++;
-      } else if (ingred == 'T') {
-        numTomatoes++;
-      }
-
-      if (numMushrooms >= minIngredients && numTomatoes >= minIngredients) {
-        // We have a slice
-        console.log('slice?', row, col);
-        s.r2 = row;
-        s.c2 = col;
-      //  console.log(s);
-
-        if (isSquare(s)) {
-          console.log('square');
-          if (numMushrooms + numTomatoes <= maxCells) {
-            slices.push(s);
-            console.log(s);
-          }
-        }
-        // console.log(pizza);
-        console.log(col + ', ' + row + ', ' + numMushrooms + ', ' + numTomatoes)
-      }
-    }
-  }
 }
 
 function findSliceVertical() {
@@ -114,11 +60,11 @@ function findSliceVertical() {
 
   for (var col = 0; col < cols; col++) {
     for (var row = 0; row < rows; row++) {
-     
-      let ingred = pizza[col][row];
+
+      let ingred = pizza[row][col];
 
       if (ingred == 'M' || ingred == 'T') {
-        cellHistory.push({ x: col, y: row });
+        cellHistory.push({ x: row, y: col });
 
         if (startedSlice === false) {
           s = new slice();
@@ -139,11 +85,8 @@ function findSliceVertical() {
         s.c2 = col;
 
         if (isSquare(cellHistory)) {
-          //console.log('SQUARE', cellHistory.length, maxCells);
           if (cellHistory.length <= maxCells) {
             slices.push(s);
-            
-            //console.log('slice pushed', s);
             fillPizza(cellHistory, slices.length);
             startedSlice = false;
             cellHistory = [];
@@ -151,31 +94,33 @@ function findSliceVertical() {
           }
         }
       }
-
     }
   }
   return slices;
 }
 
 function isSquare(ch) {
-  var maxX = 0
-  var maxY = 0
-  
+  var cells = ch;
+  var lastCell = cells.pop();
+  var maxX = 0, maxY = 0, minX = lastCell.x, minY = lastCell.y;
+  var r = 0,c = 0;
+
   ch.forEach(pc => {
-    //console.log('pc', pc);
+    if (pc.x < minX) { minX = pc.x; }
+    if (pc.y < minY) { minY = pc.y; }
     if (pc.x > maxX) { maxX = pc.x; }
     if (pc.y > maxY) { maxY = pc.y; }
   });
 
-  var lastCell = ch[ch.length - 1];
-
+  //console.log('rc',r,c);
+  //console.log('aaa', minX, maxX, minY, maxY);
+  
   if (lastCell.x === maxX && lastCell.y === maxY)
   {
     console.log('true');
     return true;
   }
-  console.log('false');
-  return false;  
+  return false;
 }
 
 function fillPizza(ch, i) {
@@ -183,5 +128,9 @@ function fillPizza(ch, i) {
   ch.forEach(pc => {
     pizza[pc.x][pc.y] = i;
   });
+  printPizza();
+}
+
+function printPizza(){
   console.log(pizza);
 }
